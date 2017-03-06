ce606d7af15587c33e2f7b12832e2d11f41a54f5
SMIT9990@pacificmags.com.au <wayne.smithers@pacificmags.com.au>
2017-03-06 14:17:59 +1100

    initital push of sample uSync 3.3.2.740 on Umbraco 7.5.10 with image id remaining as GUID after import

---
+++ Umbraco7910/Models/BannerRepository.cs
@@ -0,0 +1,33 @@
+﻿using Our.Umbraco.Vorto.Extensions;
+using Umbraco.Web;
+using Umbraco.Core.Models;
+
+namespace uSync.Site.Models
+{
+    public class BannerRepository
+    {
+        public const string DataItemProperty = "bannerData";
+        private string _alt;
+        private IPublishedContent _dataItem;
+        private string _url;
+
+        public BannerRepository(IPublishedContent model)
+        {
+            _dataItem = model.GetVortoValue<IPublishedContent>(DataItemProperty);
+            var umb = new UmbracoHelper(UmbracoContext.Current);
+            if (BannerImageExists)
+            {
+                // this parse will fail if image id has not been remapped from GUID
+                var image = umb.TypedMedia(int.Parse(GetBannerId));
+                _url = image.GetCropUrl("1200px");
+                _alt = image.Name;
+            }
+        }
+        public bool BannerImageExists => _dataItem != null && !string.IsNullOrWhiteSpace(_dataItem.GetPropertyValue<string>("bannerImage"));
+
+        public string GetBannerId => BannerImageExists ? _dataItem.GetPropertyValue<string>("bannerImage") : null;
+
+        public string Url => _url;
+        public string AltText => _alt;
+    }
+}
